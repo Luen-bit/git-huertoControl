f649ee66fc0403441c4902eb68ca743f5aea69a9
Luen <test-luis@outlook.com>
2023-04-18 15:48:38 -0600

    controladores terminados

--- node/controllers/controller.js
+++ node/controllers/controller.js
@@ -15,13 +15,55 @@ export const getAllInsumos = async (req,res) => {
 }
 
 // mostrar un registro
-
+ export const getInsumo = async(req,res) =>{
+    try {
+        const insumo = await insumoModel.findAll({
+            where : {id:req.params.id}
+        })
+        res.json(insumo)
+    } catch (error) {
+        res.json({message:error.message})
+    }
+ }
 
 // crear un registro
+ export const createInsumo = async(res,req) => {
+    try {
+        await insumoModel.create(re.body)
+        res.json({
+            "message": "Insumo credo correctamente"
+        })
+    } catch (error) {
+        res.json({message:error.message})
+    }
 
+ }
 
 //actualizar un registro
 
+export const updateInsumo = async (res,req) => {
+    try {
+        await insumoModel.update(req.body,{
+            where: { id: req.params.id}
+        })
+        res.json({
+            "message": "Insumo actualizado correctamente"
+        })
+    } catch (error) {
+        res.json({message:error.message})
+    }
+}
 
 //Eliminar un registro
-
+export const deleteInsumo = async (res,req) => {
+    try {
+        await insumoModel.destroy({
+            where: {id:req.params.id}
+        })
+        res.json({
+            "message":"Insumo eliminado"
+        })
+    } catch (error) {
+        res.json({message:error.message})
+    }
+}
